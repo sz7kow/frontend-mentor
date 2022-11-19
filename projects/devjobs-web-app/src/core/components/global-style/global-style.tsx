f8b7e654b92a280aa58f3c1a3243ddc33e9b387d
sz7kow <sz7kow@gmail.com>
2022-11-19 01:16:33 +0100

    feat(djwa): detail global-style

--- projects/devjobs-web-app/src/core/components/global-style/global-style.tsx
+++ projects/devjobs-web-app/src/core/components/global-style/global-style.tsx
@@ -2,8 +2,6 @@ import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    max-height: 100vh;
-    overflow: hidden;
     background-color: ${({theme}) => theme.palette.background.primary};
     font-family: ${({theme}) => theme.typography.fontFamily};
     font-size: ${({theme}) => theme.typography.body.fontSize};
@@ -12,8 +10,39 @@ const GlobalStyle = createGlobalStyle`
     color: ${({theme}) => theme.palette.typography.default};
   }
 
-  h1,h2,h3,h4 {
-    color: ${({theme}) => theme.palette.typography.default};
+  h1 {
+    font-size: ${({theme}) => theme.typography.h1.fontSize};
+    font-weight: ${({theme}) => theme.typography.h1.fontWeight};
+    line-height: ${({theme}) => theme.typography.h1.lineHeight};
+    color: ${({theme}) => theme.palette.typography.title};
+  }
+
+  h2 {
+    font-size: ${({theme}) => theme.typography.h2.fontSize};
+    font-weight: ${({theme}) => theme.typography.h2.fontWeight};
+    line-height: ${({theme}) => theme.typography.h2.lineHeight};
+    color: ${({theme}) => theme.palette.typography.title};
+  }
+
+  h3 {
+    font-size: ${({theme}) => theme.typography.h3.fontSize};
+    font-weight: ${({theme}) => theme.typography.h3.fontWeight};
+    line-height: ${({theme}) => theme.typography.h3.lineHeight};
+    color: ${({theme}) => theme.palette.typography.title};
+  }
+
+  h4 {
+    font-size: ${({theme}) => theme.typography.h4.fontSize};
+    font-weight: ${({theme}) => theme.typography.h4.fontWeight};
+    line-height: ${({theme}) => theme.typography.h4.lineHeight};
+    color: ${({theme}) => theme.palette.typography.title};
+  }
+
+  button {
+    font-size: ${({theme}) => theme.typography.button.fontSize};
+    font-weight: ${({theme}) => theme.typography.button.fontWeight};
+    line-height: ${({theme}) => theme.typography.button.lineHeight};
+    color: ${({theme}) => theme.palette.typography.title};
   }
 `;
 
