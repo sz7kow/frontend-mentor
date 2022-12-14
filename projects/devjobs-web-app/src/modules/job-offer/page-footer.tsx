b01c1f959a30cd7255d7652964954557c1b9fb56
sz7kow <sz7kow@gmail.com>
2022-12-14 06:20:06 +0100

    fix(djwa): fix button-link spacing inside of page-footer

--- projects/devjobs-web-app/src/modules/job-offer/page-footer.tsx
+++ projects/devjobs-web-app/src/modules/job-offer/page-footer.tsx
@@ -12,11 +12,11 @@ export const PageFooter: React.FC<Props> = ({applyUrl, companyName, position}) =
   <Paper>
     <footer className="py-6 tablet:py-5.75">
       <Container className="flex flex-col tablet:flex-row tablet:items-center tablet:justify-between" size="medium">
-        <div className="hidden tablet:flex tablet:flex-col tablet:gap-y-1.5">
+        <div className="hidden tablet:flex tablet:grow tablet:flex-col tablet:gap-y-1.5">
           <h3 className="text-h3">{position}</h3>
           <p className="text-base text-gray-700 dark:text-gray-500">{companyName}</p>
         </div>
-        <ButtonLink className="w-full" href={applyUrl}>
+        <ButtonLink className="w-full tablet:w-auto" href={applyUrl}>
           Apply Now
         </ButtonLink>
       </Container>
