0f2693e9a70f89778a38042b9fcf9147ba03d829
sz7kow <sz7kow@gmail.com>
2022-12-15 05:29:57 +0100

    feat(djwa): improve scrolling animations

--- projects/devjobs-web-app/src/pages/_app.tsx
+++ projects/devjobs-web-app/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import {domAnimation, LazyMotion} from 'framer-motion';
 import {AppType} from 'next/app';
 import Head from 'next/head';
-import {Fragment} from 'react';
+import {useRouter} from 'next/router';
+import {Fragment, useEffect} from 'react';
 
 import {withTRPC} from '~/app/trpc-next-instance';
 import {PageAnimationProvider} from '~/job-offer/page-animation-provider';
@@ -10,21 +11,33 @@ import {ThemeProvider} from '~/theme/theme-provider';
 
 import '~/app/tailwind-styles.css';
 
-const App: AppType = ({Component, pageProps}) => (
-  <Fragment>
-    <Head>
-      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    </Head>
-    <ThemeProvider>
-      <LazyMotion features={domAnimation} strict>
-        <PageHeader />
-        <PageAnimationProvider>
-          <Component {...pageProps} />
-        </PageAnimationProvider>
-      </LazyMotion>
-    </ThemeProvider>
-  </Fragment>
-);
+const App: AppType = ({Component, pageProps}) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.beforePopState(({as, options, url}) => {
+      router.push(url, as, {...options, scroll: false});
+
+      return true;
+    });
+  }, [router]);
+
+  return (
+    <Fragment>
+      <Head>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      </Head>
+      <ThemeProvider>
+        <LazyMotion features={domAnimation} strict>
+          <PageHeader />
+          <PageAnimationProvider>
+            <Component {...pageProps} />
+          </PageAnimationProvider>
+        </LazyMotion>
+      </ThemeProvider>
+    </Fragment>
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default withTRPC(App);
